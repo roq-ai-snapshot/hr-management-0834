af3e0f1d618c0a51b630885d1b7f8ab631343b3a
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-07-11 00:36:01 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,8 @@
+export const appConfig = {
+  ownerRoles: ['Owner'],
+  customerRoles: ['Customer'],
+  tenantRoles: ['Owner'],
+  tenantName: 'Organization',
+  applicationName: ' HR Management',
+  addOns: ['notifications', 'chat', 'file'],
+} as const;
